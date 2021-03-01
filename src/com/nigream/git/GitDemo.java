cf845736943655de04acded88d41c152488e5524
Nigream <nigreamlin@gmail.com>
2021-03-01 23:04:36 +0800

    创建项目，并编写主程序。

---
+++ src/com/nigream/git/GitDemo.java
@@ -0,0 +1,19 @@
+package com.nigream.git;
+
+/**
+ * @Author Nigream
+ * @Describe:
+ * @Date 2021/3/1 22:48
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+        System.out.println("Hello Git Project!!!");
+    }
+}
